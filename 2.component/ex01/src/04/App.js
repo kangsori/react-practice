d4c8eaaa7a1520b47090225b92e5fe34ad15e17d
kangsori <rkdthfl22@naver.com>
2023-03-09 09:40:06 +0900

    component property

--- 2.component/ex01/src/04/App.js
+++ 2.component/ex01/src/04/App.js
@@ -6,7 +6,14 @@ function App() {
     <div id='App'>
       <MyComponent 
         props01={'문자열'}
-        props02={2}/>
+        props02={2}
+        props03={true}
+        props04={{no:1, name:'둘리'}}
+        props05={['hello','wordl','react.js','es6']}
+        props06={() => '함수'}
+        props07={10}
+        props08={[true,false,true]}
+        props09={{no:1, name:'둘리', email:'dooly@gamil'}}/>
     </div>
   );
 }
