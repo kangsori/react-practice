7b6ab71cdc7d73ccf834dcae821000eb65052f62
kangsori <rkdthfl22@naver.com>
2023-03-09 14:50:34 +0900

    kanbanboard #2

--- kanbanboard/frontend/src/App.js
+++ kanbanboard/frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './assets/css/App.css';
+import KanbanBoard from './KanbanBoard';
 
 function App(props) {
     return (
-        <div id='App'>
-            <h1 >Kanban Board App</h1>
+        <div id='App' className={'App'}>
+            <KanbanBoard />
         </div>
     );
 }
