2d171ded91210ea66895443a266d4cb0e9393eaf
kangsori <rkdthfl22@naver.com>
2023-03-13 16:53:10 +0900

    commit

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import emaillist from './assets/json/data.json';
+import React, { useState } from 'react';
+import data from './assets/json/data.json';
 import styles from './assets/css/App.css';
 import Emaillist from './Emaillist';
 import RegisterForm from './RegisterForm';
 import Searchbar from './Searchbar';
 
 function App(props) {
+    const [emails, setEmails] = useState(data);
+    const notifyKeywordChanged = function(keyword){
+        // keyword가 firstName or lastName or email
+        console.log(keyword);
+        const newEmails = data.filter(function(e){
+            return (e.firstName+e.lastName).includes(keyword) || e.email.includes(keyword) ? e : null
+        })
+        setEmails(newEmails);
+    }
     return (
         <div id={styles.App}>
             <RegisterForm />
-            <Searchbar />
-            <Emaillist emaillist={emaillist}/>
+            <Searchbar callback={notifyKeywordChanged}/>
+            <Emaillist emaillist={emails}/>
         </div>
     );
 }
