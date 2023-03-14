920244452c36bf67661f25244f597f2f94447cf4
kangsori <rkdthfl22@naver.com>
2023-03-14 11:15:27 +0900

    clock #1

---
+++ 2.component/ex05/src/04/App.js
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from 'react';
+import './assets/scss/App.scss'
+import Clock from './Clock';
+
+export default function App() {
+    const [ticks, setTicks] = useState(0);
+
+    return (
+        <div>
+            <span>{ticks}</span>
+            <Clock
+                message={'ex05: useEffect Hook example'}
+                hours={'10'}
+                minutes={'20'}
+                seconds={'20'}/>
+        </div>
+            
+    );
+}
